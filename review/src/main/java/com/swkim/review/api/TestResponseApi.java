987e287d8130a50a95a507495ea4f05587081210
ksw06086 <k-sunwo@naver.com>
2023-07-11 20:06:27 +0900

    restaurant_review - Response Body

---
+++ review/src/main/java/com/swkim/review/api/TestResponseApi.java
@@ -0,0 +1,44 @@
+package com.swkim.review.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestResponseApi {
+
+    @GetMapping("/test/response/string")
+    public String stringResponse() {
+        return "This is String";
+    }
+
+    @GetMapping("/test/response/json")
+    public TestResponseBody jsonResponse() {
+        return new TestResponseBody("papa", 20);
+    }
+
+    public static class TestResponseBody {
+        String name;
+        Integer age;
+
+        public TestResponseBody(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+    }
+}
