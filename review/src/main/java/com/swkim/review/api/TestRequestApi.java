cbfede7e0f6ade9b5feb06f2b6f0fa80079ba4f2
ksw06086 <k-sunwo@naver.com>
2023-07-11 19:58:24 +0900

    restaurant_review - Request Body

--- review/src/main/java/com/swkim/review/api/TestRequestApi.java
+++ review/src/main/java/com/swkim/review/api/TestRequestApi.java
@@ -22,4 +22,23 @@ public class TestRequestApi {
     ) {
         return "Hello, Path Variable, I am " + name + ", " + age;
     }
+
+    // Request Body 방식(postman에서 body -> raw(JSON)에서 값 넣어주어야 함)
+    @PostMapping("/test/body")
+    public String requestBody(
+            @RequestBody TestRequestBody request
+    ) {
+        return "Hello, Request Body, I am " + request.name + ", " + request.age;
+    }
+
+    // reqeustBody 사용시 값을 초기화 하는 생성자를 꼭 만들어주어야함
+    public static class TestRequestBody {
+        String name;
+        Integer age;
+
+        public TestRequestBody(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
 }
